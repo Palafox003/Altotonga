94ca08e1681d4de816b5f441e8d4bd1203321c0d
Palafox003 <israelpalafox@utgz.edu.mx>
2018-04-21 16:39:00 -0500

    face
    face

--- js/post.js
+++ js/post.js
@@ -10,7 +10,16 @@ $(document).ready(function(){
 			url:'https://jsonplaceholder.typicode.com/posts',
 			success:function(res){
 				$.each(res,function(indice,post){
-					$("#posts").append('<div class="row"><div class="col"></div></div>');
+					$("#posts").append(
+								'<div class="card">'
+								  +'<h5 class="card-header">Facebook</h5>'
+								  +'<div class="card-body">'
+								    +'<h5 class="card-title">'+post.title+'</h5>'
+								    +'<p class="card-text">'+post.body+'</p>'
+								    +'<a href="#" class="btn btn-primary">Go somewhere</a>'
+								  +'</div>'
+								+'</div>'
+								);
 				});
 			}
 		});
