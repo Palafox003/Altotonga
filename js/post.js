f8d5ea2ef8f4b526555158d1085b3bf101c12bda
Palafox003 <israelpalafox@utgz.edu.mx>
2018-04-23 08:17:57 -0500

    Actualización final
    Hola adjunto la ultima actualización en donde ya se muestra el nombre de usuario por post, si hay alguna duda con el código pueden hacer sus comentarios.
    
    Saludos.

--- js/post.js
+++ js/post.js
@@ -2,6 +2,25 @@ $(document).ready(function(){
 
 
 	getPosts();
+	getUsers();
+
+	function getUsers(){
+		$.ajax({
+			dataType:'json',
+			type:'get',
+			url:'https://jsonplaceholder.typicode.com/users',
+			success:function(res){
+				$(".userName").each(function(index){
+					var userId=$(this).attr("id");
+						$.each(res,function(indice,user){
+							if(userId==user.id){
+								$(".userName").eq(index).text(user.name);
+							}
+						});
+				});
+			}
+		});
+	}	
 
 	function getPosts(){
 		$.ajax({
@@ -11,14 +30,17 @@ $(document).ready(function(){
 			success:function(res){
 				$.each(res,function(indice,post){
 					$("#posts").append(
-								'<div class="card">'
-								  +'<h5 class="card-header">Facebook</h5>'
+								'<div class="row">'//inicia la primera fila
+								+'<div class="col-3"><img src="img/perfil.jpg" class="img-fluid" alt="Responsive image"></div>'
+								+'<div class="col">'
+								+'<div class="card">'
+								  +'<h5 class="card-header userName" id="'+post.userId+'">Facebook</h5>'
 								  +'<div class="card-body">'
 								    +'<h5 class="card-title">'+post.title+'</h5>'
 								    +'<p class="card-text">'+post.body+'</p>'
 								    +'<a href="#" class="btn btn-primary">Go somewhere</a>'
 								  +'</div>'
-								+'</div>'
+								+'</div></div></div>'
 								);
 				});
 			}
